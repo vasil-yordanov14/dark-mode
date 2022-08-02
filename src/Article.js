81a8a7c91cb12b41718581eca9bfc08f898c6240
vasil-yordanov14 <vasil_sh@abv.bg>
2022-08-02 20:24:10 +0300

    set date

--- src/Article.js
+++ src/Article.js
@@ -1,7 +1,17 @@
-import React from 'react'
-import moment from 'moment'
-const Article = () => {
-  return <h2>article component</h2>
-}
+import React from "react";
+import moment from "moment";
+const Article = ({ title, snippet, date, length }) => {
+  console.log(date);
+  return (
+    <article className="post">
+      <h2>{title}</h2>
+      <div className="post-info">
+        <span>{moment(date).format(`MMMM dddd Do, YYYY`)}</span>
+        <span>{length} min read</span>
+      </div>
+      <p>{snippet}</p>
+    </article>
+  );
+};
 
-export default Article
+export default Article;
